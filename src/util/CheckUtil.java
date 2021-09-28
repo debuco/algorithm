788af915725bac941d8debe7b9893dfccb346b90
debuco <570463767@qq.com>
2021-09-28 16:23:36 +0800

    src/chapter1

---
+++ src/util/CheckUtil.java
@@ -0,0 +1,14 @@
+package util;
+
+/**
+ * @author bxwang
+ * @date 2021/9/28
+ * @apiNote 这是一个标记校验类，具体的逻辑没有实现，只提供一些标记校验的方法
+ */
+public class CheckUtil {
+    public static void checkCapacity() throws IllegalStateException{}
+
+    public static void checkType() throws ClassCastException{}
+
+    public static void checkTypeAndCapacity() {}
+}
