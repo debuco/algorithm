4dc0c35f571ce00ef66471dd813e61f4e872fbe2
debuco <570463767@qq.com>
2021-10-13 11:27:34 +0800

    add directed graph

---
+++ src/chapter4/directedgraph/Topological.java
@@ -0,0 +1,8 @@
+package chapter4.directedgraph;
+
+/**
+ * @author bxwang(570463767 @ qq.com)
+ * @date 2021/10/13 9:58
+ */
+public class Topological {
+}
---
+++ src/chapter4/directedgraph/impl/TopologicalImpl.java
@@ -0,0 +1,8 @@
+package chapter4.directedgraph.impl;
+
+/**
+ * @author bxwang(570463767 @ qq.com)
+ * @date 2021/10/13 10:53
+ */
+public class TopologicalImpl {
+}
